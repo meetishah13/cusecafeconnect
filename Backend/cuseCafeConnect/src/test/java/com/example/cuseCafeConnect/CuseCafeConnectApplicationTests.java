73415b5c1b24f540041532be34049a19a310de87
Rohan Bick <rohanbick@rohans-mbp-2.mynetworksettings.com>
2024-04-06 18:49:09 -0400

    new commit by rohan

---
+++ Backend/cuseCafeConnect/src/test/java/com/example/cuseCafeConnect/CuseCafeConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuseCafeConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuseCafeConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
