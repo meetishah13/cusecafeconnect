e1e69ff313ba7acf20729a385fa20ea4e7efa9ee
Rohan Bick <rohanbick@Rohans-MacBook-Pro-2.local>
2024-04-10 18:28:33 -0400

    cafeapiupd

--- backend/cuseCafeConnect/src/main/java/com/example/cuseCafeConnect/models/Cafe.java
+++ backend/cuseCafeConnect/src/main/java/com/example/cuseCafeConnect/models/Cafe.java
@@ -1,6 +1,17 @@
 package com.example.cuseCafeConnect.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cafe")
 public class Cafe {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int cafeID;
     private String cafeName;
     private String cafeLat;
