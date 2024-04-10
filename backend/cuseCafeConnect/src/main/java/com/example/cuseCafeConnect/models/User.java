05a3812a533a5a3c5288a256d4f2ac7ed37a8d82
Jaynam404 <jaynam.s@somaiya.edu>
2024-04-10 18:37:59 -0400

    Updated user model

--- backend/cuseCafeConnect/src/main/java/com/example/cuseCafeConnect/models/User.java
+++ backend/cuseCafeConnect/src/main/java/com/example/cuseCafeConnect/models/User.java
@@ -1,91 +1,105 @@
 package com.example.cuseCafeConnect.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
-	 private int userID;
-	    private String userEmail;
-	    private String fName;
-	    private String lName;
-	    private String password;
-	    private String phoneNo;
-	    private int roleID;
-	    private int cafeID;
-	    private byte[] photoPath;
+	@Id
+	private int userID;
+	private String userEmail;
+	private String fName;
+	private String lName;
+	private String password;
+	private String phoneNo;
+	private int roleID;
+	private int cafeID;
+	private byte[] photoPath;
 
-	    // Constructors
-	    public User() {}
+	// Constructors
+	public User() {
+	}
 
-		public int getUserID() {
-			return userID;
-		}
+	public int getUserID() {
+		return userID;
+	}
 
-		public void setUserID(int userID) {
-			this.userID = userID;
-		}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
 
-		public String getUserEmail() {
-			return userEmail;
-		}
+	public String getUserEmail() {
+		return userEmail;
+	}
 
-		public void setUserEmail(String userEmail) {
-			this.userEmail = userEmail;
-		}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
 
-		public String getfName() {
-			return fName;
-		}
+	public String getfName() {
+		return fName;
+	}
 
-		public void setfName(String fName) {
-			this.fName = fName;
-		}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
 
-		public String getlName() {
-			return lName;
-		}
+	public String getlName() {
+		return lName;
+	}
 
-		public void setlName(String lName) {
-			this.lName = lName;
-		}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
 
-		public String getPassword() {
-			return password;
-		}
+	public String getPassword() {
+		return password;
+	}
 
-		public void setPassword(String password) {
-			this.password = password;
-		}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-		public String getPhoneNo() {
-			return phoneNo;
-		}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
 
-		public void setPhoneNo(String phoneNo) {
-			this.phoneNo = phoneNo;
-		}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
 
-		public int getRoleID() {
-			return roleID;
-		}
+	public int getRoleID() {
+		return roleID;
+	}
 
-		public void setRoleID(int roleID) {
-			this.roleID = roleID;
-		}
+	public void setRoleID(int roleID) {
+		this.roleID = roleID;
+	}
 
-		public int getCafeID() {
-			return cafeID;
-		}
+	public int getCafeID() {
+		return cafeID;
+	}
 
-		public void setCafeID(int cafeID) {
-			this.cafeID = cafeID;
-		}
+	public void setCafeID(int cafeID) {
+		this.cafeID = cafeID;
+	}
 
-		public byte[] getPhotoPath() {
-			return photoPath;
-		}
+	public byte[] getPhotoPath() {
+		return photoPath;
+	}
 
-		public void setPhotoPath(byte[] photoPath) {
-			this.photoPath = photoPath;
-		}
-	    
-	    
+	public void setPhotoPath(byte[] photoPath) {
+		this.photoPath = photoPath;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userID=" + userID + ", userEmail=" + userEmail + ", fName=" + fName + ", lName=" + lName
+				+ ", password=" + password + ", phoneNo=" + phoneNo + ", roleID=" + roleID + ", cafeID=" + cafeID + "]";
+	}
+
+
 
 }
