42e9a69c9dff5ff0d7ccad839ba5a3ac89ffd88a
Jaynam404 <jaynam.s@somaiya.edu>
2024-05-01 00:59:12 -0400

    sub book filter updated

--- backend/cuseCafeConnect/src/main/java/com/example/cuseCafeConnect/repositories/SubBookRepository.java
+++ backend/cuseCafeConnect/src/main/java/com/example/cuseCafeConnect/repositories/SubBookRepository.java
@@ -8,15 +8,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SubBookRepository extends JpaRepository<SubBook, Integer> {
-    @Query("SELECT sb.subID, c.cafeName, du.userID AS dropUserId, du.fName AS dropUserName, pu.userID AS pickUpUserId, pu.fName AS pickUpUserName, ts.timeSlotID, ts.timeSlot, ts.timeSlotDay, sb.dropDate,sb.acceptSub "
+    @Query("SELECT sb.subID, c.cafeName, du.userID AS dropUserId, du.fName AS dropUserName, pu.userID AS pickUpUserId, pu.fName AS pickUpUserName, ts.timeSlotID, ts.timeSlot, ts.timeSlotDay, sb.dropDate,sb.acceptSub , c.cafeID, sch.scheduleID "
             +
             "FROM SubBook sb " +
             "JOIN sb.cafe c " +
             "LEFT JOIN sb.dropUser du " +
             "LEFT JOIN sb.pickUpUser pu " +
             "LEFT JOIN sb.schedule sch " +
-            "LEFT JOIN sch.timeslot ts " +
-            "WHERE sb.pickUpUser IS NULL OR sb.acceptSub = 0")
+            "LEFT JOIN sch.timeslot ts " )
+            //"WHERE sb.pickUpUser IS NULL OR sb.acceptSub = 0")
     List<Object[]> findSubBooksByPickUpUserIsNullOrAcceptSub();
 
     @Query("SELECT sb.subID, c.cafeName, du.userID AS dropUserId, du.fName AS dropUserName, pu.userID AS pickUpUserId, pu.fName AS pickUpUserName, ts.timeSlotID, ts.timeSlot, ts.timeSlotDay, sb.dropDate,sb.acceptSub,sb.comments "
